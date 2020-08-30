7780b963719c35e1a7c48061b04d96f0d57f2134
Andrew <designwebmoldova@gmail.com>
2020-08-30 15:02:17 +0300

    lucrarea de lab cu functii

---
+++ lab2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let e = 20;
+// console.log(e);
+
+function varExample() {
+  let e = 1;
+  
+  function subFunction(){
+    console.log(e);
+  }
+  
+  let subFunction = subFunction{};
+}
+
+// varExample();
+console.log(subFunction);
+// console.log(e);
